8c2c5dea7e5c798b12627a0ae58b0d036c686240
RussianHustla <maximilyn10@mail.ru>
2022-06-03 22:06:25 +0300

    api config changed

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,3 @@
-export const PORT = '4000'
-export const BASE_URL = `http://localhost:${PORT}`
+export const PORT = '4000' // Порт, на котором запущен бэкенд-сервер
+// export const BASE_URL = `http://localhost:${PORT}`
+export const BASE_URL = `http://192.168.0.107:${PORT}` // ip адрес виртуальной машины в локальной сети
